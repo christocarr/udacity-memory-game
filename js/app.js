8f8a535ba529694aeb858ef435075fcd536482ed
christocarr <christocarr@outlook.com>
2018-03-12 21:21:31 +0000

    change cards to list items

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 //get cards with class name card and make array called cards
-let allCards = document.querySelectorAll('.card');
+const allCards = document.querySelectorAll('.card');
 const cards = [...allCards];
 
 //add event listener for each card in cards array
 cards.forEach(function(card) {
 	card.addEventListener('click', function (){
 		console.log('card clicked');
+		
 	})
 });
+
