6bc3e73d647f425ae0f9be60c126c71817d62c3d
christocarr <christocarr@outlook.com>
2018-03-17 10:38:07 +0000

    added shuffle cards functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 //get cards with class name card and make array called cards
-const allCards = document.querySelectorAll('.card');
-const cards = [...allCards];
+const allCards = document.getElementsByClassName('card');
+let cards = [...allCards];
 
 //add event listener for each card in cards array
 cards.forEach(function(card) {
@@ -10,4 +10,20 @@ cards.forEach(function(card) {
 		
 	})
 });
+
+//function to shuffle cards at on load and 
+function shuffle(arr) {
+	let randomCard;
+	let tempCard; //for swopping
+	for (let i = arr.length-1; i >= 0; i--) {
+		
+		randomCard = Math.floor(Math.random() * i); //gets random card in array
+		
+		tempCard = arr[i];
+		arr[i] = arr[randomCard];
+		arr[randomCard] = tempCard;
+	}
+	
+	return arr;
+}
 
